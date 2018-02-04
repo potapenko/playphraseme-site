d6436189891e35ec706f77f81ac462696345dac0
eugene_potapenko <>
2018-02-04 15:38:58 +0100

    model

--- src/cljs/playphraseme/model.cljs
+++ src/cljs/playphraseme/model.cljs
@@ -2,22 +2,23 @@
   (:require [re-frame.core :refer [dispatch reg-event-db reg-sub]]
             [playphraseme.common.localstorage :as localstorage]))
 
-;;dispatchers
+(reg-event-db
+ :initialize-db
+ (fn [_ _]
+   {:page :search}))
 
 (reg-event-db
-  :initialize-db
-  (fn [_ _]
-    {:page :search}))
+ :set-active-page
+ (fn [db [_ page params]]
+   (println "params:" page params)
+   (assoc db
+          :page page
+          :params params)))
 
 (reg-event-db
-  :set-active-page
-  (fn [db [_ page]]
-    (assoc db :page page)))
-
-(reg-event-db
-  :set-docs
-  (fn [db [_ docs]]
-    (assoc db :docs docs)))
+ :set-docs
+ (fn [db [_ docs]]
+   (assoc db :docs docs)))
 
 (reg-event-db
  :locale
@@ -25,19 +26,73 @@
  (fn [db [_ docs]]
    (assoc db :locale docs)))
 
-;;subscriptions
+(reg-sub
+ :page
+ (fn [db _]
+   (:page db)))
 
 (reg-sub
-  :page
-  (fn [db _]
-    (:page db)))
+ :params
+ (fn [db _]
+   (:params db)))
 
 (reg-sub
-  :docs
-  (fn [db _]
-    (:docs db)))
+ :docs
+ (fn [db _]
+   (:docs db)))
 
 (reg-sub
  :locale
  (fn [db _]
    (get db :locale :en)))
+
+(reg-event-db
+ :set-auth-data
+ (fn [db [_ value]]
+   (if value
+     (assoc db :auth-data
+            (update value :permissions #(set (map keyword %))))
+     (dissoc db :auth-data))))
+
+(defn- user-roles [db]
+  (get-in db [:auth-data :permissions] #{}))
+
+(reg-sub
+ :user-roles
+ (fn [db _]
+   (user-roles db)))
+
+(reg-sub
+ :logged?
+ (fn [db _]
+   (-> db :auth-data nil? not)))
+
+(reg-sub
+ :has-user-role?
+ (fn [db [_ role]]
+   (-> ((user-roles db) role) nil? not)))
+
+(reg-sub
+ :page-before-login
+ (fn [db _]
+   (get db :page-before-login)))
+
+(reg-event-db
+ :page-before-login
+ (fn [db [_ value]]
+   (assoc db :page-before-login value)))
+
+(reg-sub
+ :mobile?
+ (fn [db _]
+   (:mobile? db)))
+
+(reg-event-db
+ :mobile?
+ (fn [db [_ value]]
+   (assoc db :mobile? value)))
+
+(reg-sub
+ :desktop?
+ (fn [db _]
+   (not (:mobile? db))))
