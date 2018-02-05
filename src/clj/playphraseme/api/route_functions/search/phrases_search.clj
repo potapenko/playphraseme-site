2e336a8861b39d11e58b5a87089a8cca4de774d2
eugene_potapenko <>
2018-02-05 20:26:14 +0100

    search phrases in progress

--- src/clj/playphraseme/api/route_functions/search/phrases_search.clj
+++ src/clj/playphraseme/api/route_functions/search/phrases_search.clj
@@ -1,11 +1,25 @@
 (ns playphraseme.api.route-functions.search.phrases-search
   (:require [ring.util.http-response :refer :all]
             [clj-http.client :as http]
-            ))
+            [clojure.core.async :refer [<! >! chan close! go go-loop put! timeout]]
+            [playphraseme.app.config :refer [env]]
+            [cheshire.core :refer :all]
+            [clojure.string :as string]))
+
+(defn get-phrase-data [id]
+  id)
+
+(defn get-phrases [phrases-ids]
+  (map get-phrase-data phrases-ids))
 
 (defn search-response [q skip limit]
+  (let [url (str (:indexer-url env) "/search")
+        res (http/get url {:query-params {:q q :skip skip :limit limit} :accept :json})]
+    (some-> res :body (parse-string true) (update :phrases get-phrases))))
 
+(comment
 
+  (search-response "hello" 0 10)
 
 
   )
