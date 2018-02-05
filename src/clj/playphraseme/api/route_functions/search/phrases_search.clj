a45a42bb227d8e4f8e295ba7345a3cc41c170215
eugene_potapenko <>
2018-02-05 19:56:12 +0100

    in progress

--- src/clj/playphraseme/api/route_functions/search/phrases_search.clj
+++ src/clj/playphraseme/api/route_functions/search/phrases_search.clj
@@ -1,6 +1,7 @@
 (ns playphraseme.api.route-functions.search.phrases-search
   (:require [ring.util.http-response :refer :all]
-            [clj-http.client :as http]))
+            [clj-http.client :as http]
+            ))
 
 (defn search-response [q skip limit]
 
