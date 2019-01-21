f59e5fe8161c8d16fc8ed353797b75e3c5c4a964
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2019-01-21 13:30:35 +0100

    google search optimizations

---
+++ src/clj/playphraseme/common/google_search.clj
@@ -0,0 +1,12 @@
+(ns playphraseme.common.google-search)
+
+(def default-title "PlayPhrase.me: Endless stream of movie clips of specific phrases")
+(def default-description "Look for phrases in movies and watch videos with them.")
+(def default-search-text "hello")
+
+;; count of found video
+(defn generate-page-title [search-text])
+
+(defn generate-page-description [search-text])
+
+(defn generate-page-static-content [search-text])
