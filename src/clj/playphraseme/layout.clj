d090b550eb56867a7fccf326cbecc3157e619d21
Eugnene Potapenko <eugnenepotapenko@Eugnenes-MacBook-Pro.local>
2019-11-19 11:52:35 +0100

    db connected

--- src/clj/playphraseme/layout.clj
+++ src/clj/playphraseme/layout.clj
@@ -34,4 +34,4 @@
   [error-details]
   {:status  (:status error-details)
    :headers {"Content-Type" "text/html; charset=utf-8"}
-   :body    (parser/render-file "error.html" error-details)})
+   :body    "" #_(parser/render-file "error.html" error-details)})
